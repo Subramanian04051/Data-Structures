7aa9f1738b0e079eeb81b11fcf012e7341928254
Subramanian Muthu kumar <subramanian04051@gmail.com>
2024-05-27 15:36:36 +0530

    Binary Max Heap implementation in javascript

---
+++ BinaryMaxHeap.js
@@ -0,0 +1,42 @@
+class BinaryMaxHeap
+{
+    constructor()
+    {
+        this.arr=[]
+    }
+    Parent(i)
+    {
+        return ((i-1)/2)
+    }
+    getMax()
+    {
+        return this.arr[0]
+    }
+    insert(element)
+    {
+        this.arr.push(element)
+        let i=this.arr.length-1
+       // console.log(i)
+        while(i>0 && this.arr[this.Parent(i)]<this.arr[i])
+        {
+            let p=this.Parent(i),temp
+            console.log(p)
+            temp=this.arr[i]
+            this.arr[i]=this.arr[p]
+            this.arr[p]=temp
+            //this.arr[i],this.arr[p]=this.swap(this.arr[p],this.arr[i])
+            i=p
+        }
+    }
+}
+
+
+var bt=new BinaryMaxHeap()
+bt.insert(3)
+console.log(bt.arr)
+bt.insert(2)
+console.log(bt.arr)
+bt.insert(1)
+bt.insert(1)
+console.log(bt.arr)
+console.log(bt.getMax())
